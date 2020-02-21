1c19d4c11b85a3dd6dc06084db70c867ed97f8c7
sr4p <srsai.sr4p@hotmail.com>
2020-02-21 12:08:50 +0700

    ooad

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
       <b-navbar-brand href="#"><b-icon icon="image" font-scale="2.5"></b-icon></b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <b-nav vertical class="w-25">
+    <b-nav vertical class="w-15">
       <b-nav-item active><router-link to="/">Home</router-link></b-nav-item>
+      <b-card header="การจัดการ">
+    <b-list-group>
+      <b-list-group-item href="#" style="text-align: center; font-size: 15px"><b-nav-item><router-link to="/student">Student</router-link></b-nav-item></b-list-group-item>
+      <b-list-group-item href="#" style="text-align: center; font-size: 15px"><b-nav-item><router-link to="/partner">Partner</router-link></b-nav-item></b-list-group-item>
+      <b-list-group-item href="#" style="text-align: center; font-size: 15px"><b-nav-item><router-link to="/teacher">Teacher</router-link></b-nav-item></b-list-group-item>
+    </b-list-group>
+  </b-card>
+      <!-- <b-nav-item active><router-link to="/">Home</router-link></b-nav-item>
       <b-nav-item><router-link to="/student">Student</router-link></b-nav-item>
       <b-nav-item><router-link to="/partner">Partner</router-link></b-nav-item>
-      <b-nav-item><router-link to="/teacher">Teacher</router-link></b-nav-item>
+      <b-nav-item><router-link to="/teacher">Teacher</router-link></b-nav-item> -->
     </b-nav>
   </div>
 </template>
